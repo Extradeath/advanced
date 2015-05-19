379f13ebb9eb5420205ad624013460b6af935d26
TheFenderStory <jared@thekards.com>
2015-05-18 17:23:01 -0700

    Update mongo.js

--- infinite/mongo.js
+++ infinite/mongo.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
  */
 
  exports.connect = function(db) {
-    if (!db) db = 'mongodb://naten2006:marlboro1@ds031628.Mongolab.com:31628/oasismaster';
+    if (!db) db = 'mongodb://naten2006:Marlboro1@ds031628.Mongolab.com:31628/oasismaster';
      var url = process.env.MONGODB || db;
      mongoose.connect(url);
      mongoose.connection.on('error', function() {
