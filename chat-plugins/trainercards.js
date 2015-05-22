5134a9a6208cf82de8e086ff4846e842919913c4
TheFenderStory <jared@thekards.com>
2015-05-22 06:58:52 -0700

    Update trainercards.js

--- chat-plugins/trainercards.js
+++ chat-plugins/trainercards.js
@@ -11,13 +11,6 @@ exports.commands = {
         return this.sendReplyBox('Fender');
     },
     
-    serperiorette: 'serperiorette',
-    serperiorette: function(target, room, user) {
-        if (!this.canBroadcast()) return;
-        this.sendReplyBox('<center><img src="http://puu.sh/hVfrz/cf838762ec.png" width="378" height="430"><br><font size="3" color="black"><b>Ace(s):</b> <font color="cyan">Suicune</font> and <font color="green">Serperior</font></font><br>"You'll be the water that helps me grow stronger."  
-')
-    },
-
     /*********************************************************
      * TC'S STOP HERE!
      *********************************************************/
